6693bf5c847b5106b22134cfcec501b7733807b4
bekjon <bekjonabdurasulov21@gmail.com>
2024-11-07 18:29:49 +0500

    logon tugrlandi settingsda

--- config/settings/auth.py
+++ config/settings/auth.py
@@ -1,5 +1,7 @@
 
 AUTH_USER_MODEL = 'users.CustomUser'
+LOGIN_URL = 'login-page'
+LOGIN_REDIRECT_URL = 'home-page'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -18,3 +20,4 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
